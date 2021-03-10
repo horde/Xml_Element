9129aa230384f837658435272c3074ae8c69734d
Marco Abbrancati <abbrancati@b1-systems.de>
2021-03-10 07:48:20 +0000

    WIP 2 errors left

--- test/Horde/Xml/Element/CountTest.php
+++ test/Horde/Xml/Element/CountTest.php
@@ -8,16 +8,32 @@
  */
 namespace Horde\Xml\Element;
 use \PHPUnit\Framework\TestCase;
-use \CountTest_List;
+use \Horde_Xml_Element_List;
 
 class CountTest extends TestCase
 {
     public function testCount()
     {
-        $l = new CountTest_List(
+        $l = new Horde_CountTest_List(
             '<?xml version="1.0" encoding="utf-8"?><list><item>1</item><item>2</item></list>'
         );
         $this->assertEquals(2, $l->count(), 'List count should be 2');
+    }
+
+}
+
+class Horde_CountTest_List extends Horde_Xml_Element_List
+{
+    protected function _buildListItemCache()
+    {
+        $results = array();
+        foreach ($this->_element->childNodes as $child) {
+            if ($child->localName == 'item') {
+                $results[] = $child;
+            }
+        }
+
+        return $results;
     }
 
 }
